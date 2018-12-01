2c99848f67ac345fc8da82593e446f4d0d3a0411
VincentTran1994 <trancongvuit@gmail.com>
2018-12-01 12:26:59 -0500

    update server.php

--- server.php
+++ server.php
@@ -3,9 +3,9 @@
 	$username = "vtran32";
 	$password = "vtran32";
 	$DB = "vtran32";
+	
 	//creat connection
 	$conn = new mysqli($servername, $username, $password, $DB);
-	
 	//adding to the database
 	/*
 	insert($database,$query);
@@ -46,29 +46,77 @@
 	/*
 	$sql = "SELECT id, firstname, lastname FROM MyGuests";
 	viewDB($conn,"select * from cars");
+	// echo getOne($conn,"select * from cars where yearMake=2018 limit 1","color");
 	*/
-	function getOne($conn, $query,$keyName){
+	function getOne($conn,$query,$keyName){
+		$keyValue;
 		$result = $conn->query($query);
 		if($result) echo "<script>console.log('DB connected successfully!')</script>";
 		else echo "<script>console.log('Error: " .mysqli_error($conn)."')</script>";
 
 		if ($result->num_rows > 0) {
-
 		    // output data of each row
+
 		    $i=0;
 		    while($row = $result->fetch_assoc()) {
-		    	echo "<script>console.log('" .$i ."')</script>";
-		    	echo "<script>console.log('".$keyName .":  " . $row[$keyName] ."')</script>";
-		    	// echo "<script>console.log('color:  " . $row["color"] ."')</script>";
-		     	// echo "<script>console.log('yearMake:  " . $row["yearMake"] ."')</script>";
-		        $i++;
+		    	if(is_null($row[$keyName])){
+		    	$keyValue = "Error: Check Your Key!";
+			    } 
+			    else{
+			    	echo "<script>console.log('" .$i ."')</script>";
+			    	echo "<script>console.log('".$keyName .":  " . $row[$keyName] ."')</script>";
+			        $i++;
+			        $keyValue = $row[$keyName];
+			    } 
 		    }
 		} else {
-		    echo "0 results";
+		    $keyValue = "Error:Check your query!";
 		}
+		return $keyValue;
 	}
 
-	getOne($conn,"select * from cars where yearMake=2018 limit 1","vinNum");
+	/*function getMu($conn,$query,$rowNum)
+		purpose: select mutilple rows at the same time
+		return: will return an associated array
+		example: [
+		{   //row 1
+			'key':value,
+			'key':value
+		},
+		{
+			//row 2
+			'key':value,
+			'key':value
+		},
+		{
+			//row 3
+			'key':value,
+			'key':value
+		}
+		]
+	*/
+	function getMu($conn,$query,$rowNum){
+		$keyValue=[];
+		//checking connection!
+		$result = $conn->query($query);
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    $i=0;
+		    while($row = $result->fetch_assoc() && i < $rowNum) {
+			    // echo "<script>console.log('" .$i ."')</script>";
+			    echo "<script>console.log('" . $row[0] ."')</script>";
+			    $i++;
+			    $keyValue.push($row);
+		  	}
+		} 
+		else {
+		    echo "0 results";
+		}
+		return $keyValue;
+		
+	}
+	getMu($conn,"select * from cars",3);
+	
 
 	$conn->close();
 ?>
